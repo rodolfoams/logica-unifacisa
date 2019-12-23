807a2fef2cfebbf66c02caed5c11dcede400a8d6
Rodolfo Marinho <rodolfo@grupomarinho.com.br>
2019-12-23 19:41:42 -0300

    Adicionando inicialização do Mapa de Karnaugh.

---
+++ circuitos-logicos/karnaugh.py
@@ -0,0 +1,119 @@
+from graphics import *
+from random import randint
+from argparse import ArgumentParser
+
+
+def get_input_args():
+    parser = ArgumentParser()
+    parser.add_argument('--window_width', type=int, default=500,
+                        help="The width of the display window.")
+    parser.add_argument('--window_height', type=int, default=500,
+                        help="The height of the display window.")
+    parser.add_argument('--square_size', type=int, default=50,
+                        help="The legth of the side of each square in the Karnaugh map.")
+    return parser.parse_args()
+
+
+def get_k_map_lines(window_width, window_height, square_dim):
+    k_map_lines = []
+    for i in range(5):
+
+        # Default horizontal line
+        h_line_x_begin = window_width/2 - 2*square_dim
+        h_line_x_end = window_width/2 + 2*square_dim
+        h_line_y = window_height/2 + square_dim*(i-2)
+
+        # Default vertical line
+        v_line_x = window_width/2 + square_dim*(i-2)
+        v_line_y_begin = window_height/2 - 2*square_dim
+        v_line_y_end = window_height/2 + 2*square_dim
+
+        if i % 2 == 1:
+            h_line_x_end += square_dim
+            v_line_y_end += square_dim
+
+        elif i == 2:
+            h_line_x_begin -= square_dim
+            v_line_y_begin -= square_dim
+
+        k_map_lines.extend([
+            Line(
+                Point(h_line_x_begin, h_line_y),
+                Point(h_line_x_end, h_line_y)
+            ),
+            Line(
+                Point(v_line_x, v_line_y_begin),
+                Point(v_line_x, v_line_y_end)
+            )]
+        )
+
+    return k_map_lines
+
+
+def get_k_map_variable_labels(window_width, window_height, square_dim):
+    labels = []
+    labels.append(Text(Point(int(window_width/2 - 2.5*square_dim),
+                             window_height/2 - square_dim), "A"))
+    labels.append(Text(Point(int(window_width/2 - 2.5*square_dim),
+                             window_height/2 + square_dim), "A'"))
+
+    labels.append(
+        Text(Point(int(window_width/2 + 2.5*square_dim), window_height/2), "B"))
+    labels.append(Text(Point(int(window_width/2 + 2.5*square_dim),
+                             int(window_height/2 - 1.5*square_dim)), "B'"))
+    labels.append(Text(Point(int(window_width/2 + 2.5*square_dim),
+                             int(window_height/2 + 1.5*square_dim)), "B'"))
+
+    labels.append(Text(Point(int(window_width/2 - square_dim),
+                             window_height/2 - 2.5*square_dim), "C"))
+    labels.append(Text(Point(int(window_width/2 + square_dim),
+                             window_height/2 - 2.5*square_dim), "C'"))
+
+    labels.append(
+        Text(Point(int(window_width/2), window_height/2 + 2.5*square_dim), "D"))
+    labels.append(
+        Text(Point(int(window_width/2 - 1.5*square_dim), window_height/2 + 2.5*square_dim), "D'"))
+    labels.append(
+        Text(Point(int(window_width/2 + 1.5*square_dim), window_height/2 + 2.5*square_dim), "D'"))
+
+    return labels
+
+
+def get_k_map_values():
+    return [[randint(0, 1) for i in range(4)] for j in range(4)]
+
+
+def get_k_map_text_values(window_width, window_height, square_dim, k_map_values):
+    return [Text(Point(window_width/2 + (x-2)*square_dim + square_dim/2, window_height/2 + (y-2)*square_dim + square_dim/2), str(k_map_values[x][y])) for x in range(4) for y in range(4)]
+
+
+def main(window_width=None, window_height=None, square_dim=None):
+
+    window = GraphWin("Karnaugh Map", window_width, window_height)
+
+    window.setBackground(color_rgb(255, 255, 255))
+
+    for line in get_k_map_lines(window_width, window_height, square_dim):
+        line.setWidth(1)
+        line.setFill(color_rgb(0, 0, 0))
+        line.draw(window)
+
+    for label in get_k_map_variable_labels(window_width, window_height, square_dim):
+        label.setTextColor('black')
+        label.setSize(30)
+        label.draw(window)
+
+    k_map_values = get_k_map_values()
+    for text in get_k_map_text_values(window_width, window_height, square_dim, k_map_values):
+        text.setTextColor('black')
+        text.setSize(30)
+        text.draw(window)
+    # print(k_map_values)
+    window.getMouse()
+    window.close()
+
+
+if __name__ == "__main__":
+    args = get_input_args()
+    main(window_width=args.window_width,
+         window_height=args.window_height, square_dim=args.square_size)
